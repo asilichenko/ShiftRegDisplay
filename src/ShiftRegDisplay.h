61b44271beed5641deb3695a8a1d412972cb63b0
Oleksii Sylichenko <a.silichenko@gmail.com>
2024-01-26 23:25:04 +0200

    Update ShiftRegDisplay.h
    translate comments
    replace tabs with spaces
    update license

--- src/ShiftRegDisplay.h
+++ src/ShiftRegDisplay.h
@@ -1,7 +1,7 @@
 /*
     ShiftRegDisplay.h - Library for 9 digit 7 segment display on 74HC595 shift registers.
 
-    Copyright (C) 2021 Alexey Silichenko (a.silichenko@gmail.com)
+    Copyright (C) 2021-2024 Oleksii Sylichenko (a.silichenko@gmail.com)
   
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -23,57 +23,61 @@
 #include <Arduino.h>
 
 class ShiftRegDisplay {
+
   public:
-	static const byte MAX_LEN = 9;
 
-	enum alignMode {
-	  LOWER_POS, UPPER_POS
-	};
-	
-	enum indicatorType {
-	  COMMON_CATHODE, COMMON_ANODE
-	};
+  static const byte MAX_LEN = 9;
 
-	/**
-	 * dataPin - connected to DS (14) pin of 74HC595
-	 * latchPin - connected to ST_CP (12) and OE (13) pins of 74HC595
-	 * clockPin - connected to SH_CP (11) pin of 74HC595
-	 * bitOrder - either MSBFIRST or LSBFIRST
-	 */
-    ShiftRegDisplay(byte dataPin, byte latchPin, byte clockPin, 
-					byte bitOrder = MSBFIRST, indicatorType iType = COMMON_CATHODE);
-	
-	/**
-	 * Вывести массив символов на индикатор
-	 *
-	 * data - массив символов (размер от 0 до 9)
-	 * len - сколько символов из массива data необходимо вывести (0-9)
-	 * align - если количество символов меньше 9, то выравнивание по краю (LOWER_POS или UPPER_POS)
-	 */
-    void showData(const byte data[], byte len = MAX_LEN, alignMode align = LOWER_POS);
-	/**
-	  * Отображать данные data в течение duration миллисекунд
-	  */
-	void showDataMillis(long duration, const byte data[], byte len = MAX_LEN, alignMode align = LOWER_POS);
-	
-	/**
-	 * Вывести одиночный символ в конкретной позиции
-	 *
-	 * symbol - битовое представление символа на индикаторе
-	 * pos - позиция индикатора (0-9)
-	 */
-    void showSymbol(byte symbol, byte pos);
-	/**
-	  * Отображать одиночный символ symbol на позиции pos в течение duration миллисекунд
-	  */
-	void showSymbolMillis(long duration, byte symbol, byte pos);
+  enum alignMode {
+    LOWER_POS, UPPER_POS
+  };
+  
+  enum indicatorType {
+    COMMON_CATHODE, COMMON_ANODE
+  };
+
+  /**
+   * dataPin  - connected to DS (14) pin of 74HC595
+   * latchPin - connected to ST_CP (12) and OE (13) pins of 74HC595
+   * clockPin - connected to SH_CP (11) pin of 74HC595
+   * bitOrder - either MSBFIRST or LSBFIRST
+   */
+  ShiftRegDisplay(byte dataPin, byte latchPin, byte clockPin, 
+                  byte bitOrder = MSBFIRST, indicatorType iType = COMMON_CATHODE);
+  
+  /**
+   * Display an array of symbols to the display
+   *
+   * data - symbols array (size from 0 to 9)
+   * len - how many symbols from the array must be displayed (0-9)
+   * align - if number of symbols is less than 9, then align to the edge (LOWER_POS или UPPER_POS)
+   */
+  void showData(const byte data[], byte len = MAX_LEN, alignMode align = LOWER_POS);
+
+  /**
+   * Display `data` within `duration` milliseconds
+   */
+  void showDataMillis(long duration, const byte data[], byte len = MAX_LEN, alignMode align = LOWER_POS);
+  
+  /**
+   * Display a symbols in specific position
+   *
+   * symbol - Bit representation of the character on the display
+   * pos - indicator position (0-9)
+   */
+  void showSymbol(byte symbol, byte pos);
+
+  /**
+   * Display a symbol on the position `pos` within `duration` milliseconds
+   */
+  void showSymbolMillis(long duration, byte symbol, byte pos);
 
   private:
-	byte _dataPin;
-	byte _latchPin;
-	byte _clockPin;
-	byte _bitOrder;
-	indicatorType _iType;
-	byte Q7S_BIT;
-	void _sendData(byte symbol, byte pos);
+  byte _dataPin;
+  byte _latchPin;
+  byte _clockPin;
+  byte _bitOrder;
+  indicatorType _iType;
+  byte Q7S_BIT;
+  void _sendData(byte symbol, byte pos);
 };
