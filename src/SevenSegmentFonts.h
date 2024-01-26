c82a73b364378ae52a3a6581474299c28b7830c3
Oleksii Sylichenko <a.silichenko@gmail.com>
2024-01-26 23:07:38 +0200

    Update SevenSegmentFonts.h
    replace tabs with spaces

--- src/SevenSegmentFonts.h
+++ src/SevenSegmentFonts.h
@@ -23,17 +23,16 @@
 namespace sevenSeg {
 
 #ifndef CUSTOM_FONT_BIT_MAPPING
-	/*
-		Names of segments:
-
-		 -- A --
-		|       |
-		F       B
-		 -- G --
-		E       C
-		|       |
-		 -- D --  .DP
-	*/
+  /*
+    Names of segments:
+     -- A --
+    |       |
+    F       B
+     -- G --
+    E       C
+    |       |
+     -- D --  .DP
+  */
 #ifdef FONT_BIT_MAPPING_REVERSED
   const byte _A  bit(7);
   const byte _B  bit(6);
@@ -52,8 +51,8 @@ namespace sevenSeg {
   const byte _F  bit(5);
   const byte _G  bit(6);
   const byte _DP bit(7);
-#endif	// FONT_BIT_MAPPING_REVERSED
-#endif	// CUSTOM_FONT_BIT_MAPPING
+#endif // FONT_BIT_MAPPING_REVERSED
+#endif // CUSTOM_FONT_BIT_MAPPING
 
 #define CLEAR_BITS(val, ex)   (val ^ ex)
 
@@ -63,22 +62,22 @@ namespace sevenSeg {
   const byte DASH = _G;
 
   const byte FONT_NUMBER[16] = {
-    CLEAR_BITS(DIGIT, DASH), 	// 0
-    _B|_C, 						// 1 
-    CLEAR_BITS(DIGIT, (_F|_C)),	// 2 
-    CLEAR_BITS(DIGIT, (_F|_E)),	// 3 
-    _B|_C|_F|_G, 				// 4 
-    CLEAR_BITS(DIGIT, (_B|_E)),	// 5 
-    CLEAR_BITS(DIGIT, _B), 		// 6 
-    _A|_B|_C, 					// 7 
-    DIGIT, 						// 8
-    CLEAR_BITS(DIGIT, _E), 		// 9
-    CLEAR_BITS(DIGIT, _D), 		// A
-    CLEAR_BITS(DIGIT, (_A|_B)),	// b
-    _A|_D|_E|_F, 				// C
-    CLEAR_BITS(DIGIT, (_A|_F)),	// d
-    CLEAR_BITS(DIGIT, (_B|_C)),	// E
-    _A|_E|_F|_G 				// F
+    CLEAR_BITS(DIGIT, DASH),    // 0
+    _B|_C,                      // 1 
+    CLEAR_BITS(DIGIT, (_F|_C)), // 2 
+    CLEAR_BITS(DIGIT, (_F|_E)), // 3 
+    _B|_C|_F|_G,                // 4 
+    CLEAR_BITS(DIGIT, (_B|_E)), // 5 
+    CLEAR_BITS(DIGIT, _B),      // 6 
+    _A|_B|_C,                   // 7 
+    DIGIT,                      // 8
+    CLEAR_BITS(DIGIT, _E),      // 9
+    CLEAR_BITS(DIGIT, _D),      // A
+    CLEAR_BITS(DIGIT, (_A|_B)), // b
+    _A|_D|_E|_F,                // C
+    CLEAR_BITS(DIGIT, (_A|_F)), // d
+    CLEAR_BITS(DIGIT, (_B|_C)), // E
+    _A|_E|_F|_G                 // F
   };
 
   const byte FONT_O_SMALL = _G|_C|_D|_E;
